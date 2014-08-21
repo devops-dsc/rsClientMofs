008a2ba9337c6a8729648eb4729415f6ee9bc8db
mark4990 <mark.key@rackspace.com>
2014-08-20 20:08:30 -0500

    framework

--- DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
+++ DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
@@ -1,2 +1,28 @@
 ﻿$VerbosePreference = "Continue"
 . "C:\cloud-automation\secrets.ps1"
+
+Function Get-TargetResource {
+  param (
+    [string]$Name,
+    [Ensure]$Ensure
+  )
+  @{
+  Name = $Name
+  Ensure = $Ensure
+  }
+}
+
+Function Test-TargetResource {
+  param (
+    [string]$Name,
+    [Ensure]$Ensure
+  )
+  return $false
+}
+
+Function Set-TargetResource {
+  param (
+    [string]$Name,
+    [Ensure]$Ensure
+  )
+}
