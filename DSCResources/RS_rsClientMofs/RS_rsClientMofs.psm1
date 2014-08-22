463c4f18edd666f5be988ed9a9d7420b7c7893ad
LCZ2RCS1 <mark.key@rackspace.com>
2014-08-22 13:45:06 -0500

    added logging

--- DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
+++ DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
@@ -1,7 +1,8 @@
 Function Get-TargetResource {
    param (
       [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Name,
-      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure
+      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure,
+      [bool]$Logging
    )
    @{
   Name = $Name
@@ -11,21 +12,40 @@ Function Get-TargetResource {
 Function Test-TargetResource {
    param (
       [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Name,
-      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure
+      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure,
+      [bool]$Logging
    )
+   $logSource = $PSCmdlet.MyInvocation.MyCommand.ModuleName
    . "C:\cloud-automation\secrets.ps1"
-   $catalog = (Invoke-RestMethod -Uri $("https://identity.api.rackspacecloud.com/v2.0/tokens") -Method POST -Body $(@{"auth" = @{"RAX-KSKEY:apiKeyCredentials" = @{"username" = $($d.cU); "apiKey" = $($d.cAPI)}}} | convertTo-Json) -ContentType application/json)
+   try{
+      $catalog = (Invoke-RestMethod -Uri $("https://identity.api.rackspacecloud.com/v2.0/tokens") -Method POST -Body $(@{"auth" = @{"RAX-KSKEY:apiKeyCredentials" = @{"username" = $($d.cU); "apiKey" = $($d.cAPI)}}} | convertTo-Json) -ContentType application/json)
+   }
+   catch {
+      if($Logging) {
+         Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retreive service catalog `n $($_.Execption.Message)"
+      }
+   }
    $AuthToken = @{"X-Auth-Token"=($catalog.access.token.id)}
    $environmentGuids = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "environmentGuid") | % {($_.split("=")[1] -replace '"', "").trim()})
    $servers = @()
    foreach($environmentGuid in $environmentGuids) {
-      $servers += (((Invoke-RestMethod -Uri $("https://prefs.api.rackspacecloud.com/v1/WinDevOps", $environmentGuid -join '/') -Method GET -Headers $AuthToken -ContentType application/json).servers).servers)
+      try {
+         $servers += (((Invoke-RestMethod -Uri $("https://prefs.api.rackspacecloud.com/v1/WinDevOps", $environmentGuid -join '/') -Method GET -Headers $AuthToken -ContentType application/json).servers).servers)
+      }
+      catch {
+         Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retrieve server object from Servermill `n $($_.Exception.Message)"
+      }
    }
    $environments = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "EnvironmentName") | % {($_.split("=")[1] -replace '"', "").trim()})
    $currentConfigs = ((Get-Item -Path "C:\Program Files\WindowsPowerShell\DscService\Configuration\*").BaseName -notmatch "mof")
    foreach($currentConfig in $currentConfigs) {  
       if($servers.guid -notcontains $currentConfig) {
-         Remove-Item -Path $(($("C:\Program Files\WindowsPowerShell\DscService\Configuration", $currentConfig) -join '\'), "*" -join '')
+         try {
+            Remove-Item -Path $(($("C:\Program Files\WindowsPowerShell\DscService\Configuration", $currentConfig) -join '\'), "*" -join '')
+         }
+         catch {
+            Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to remove file `n $($_.Exception.Message)"
+         }
       }
    }
    foreach($environment in $environments) {
@@ -54,44 +74,90 @@ Function Test-TargetResource {
 Function Set-TargetResource {
    param (
       [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Name,
-      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure
+      [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure,
+      [bool]$Logging
    )
    . "C:\cloud-automation\secrets.ps1"
-   $catalog = (Invoke-RestMethod -Uri $("https://identity.api.rackspacecloud.com/v2.0/tokens") -Method POST -Body $(@{"auth" = @{"RAX-KSKEY:apiKeyCredentials" = @{"username" = $($d.cU); "apiKey" = $($d.cAPI)}}} | convertTo-Json) -ContentType application/json)
+   try {
+      $catalog = (Invoke-RestMethod -Uri $("https://identity.api.rackspacecloud.com/v2.0/tokens") -Method POST -Body $(@{"auth" = @{"RAX-KSKEY:apiKeyCredentials" = @{"username" = $($d.cU); "apiKey" = $($d.cAPI)}}} | convertTo-Json) -ContentType application/json)
+   }
+   catch {
+      Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retreive service catalog `n $($_.Execption.Message)"
+   }
    $AuthToken = @{"X-Auth-Token"=($catalog.access.token.id)}
    $monitoruri = (($catalog.access.serviceCatalog | Where-Object Name -Match "cloudMonitoring").endpoints).publicURL
    $tokenuri = ($monitoruri, "agent_tokens" -join '/')
    $environmentGuids = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "environmentGuid") | % {($_.split("=")[1] -replace '"', "").trim()})
    $servers = @()
    foreach($environmentGuid in $environmentGuids) {
-      $servers += (((Invoke-RestMethod -Uri $("https://prefs.api.rackspacecloud.com/v1/WinDevOps", $environmentGuid -join '/') -Method GET -Headers $AuthToken -ContentType application/json).servers).servers)
+      try {
+         $servers += (((Invoke-RestMethod -Uri $("https://prefs.api.rackspacecloud.com/v1/WinDevOps", $environmentGuid -join '/') -Method GET -Headers $AuthToken -ContentType application/json).servers).servers)
+      }
+      catch {
+         Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retrieve server object from Servermill `n $($_.Exception.Message)"
+      }
    }
-   $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+   try {
+      $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+   }
+   catch {
+      Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retrieve monitoring tokens `n $($_.Exception.Message)"
+   }
    foreach($server in $servers) {
       if($server.serverName) {
          if($(($agent_tokens | ? {$_.label -eq $($server.guid)}).id).count -gt 1) {
             $tokens = $($agent_tokens | ? {$_.label -eq $($server.guid)}).id
             foreach($token in $tokens) {
                $deleteTokenUri = $($tokenuri, $token -join '/')
-               Invoke-RestMethod -Uri $deleteTokenUri -Method DELETE -Headers $AuthToken  -ContentType application/json
+               try {
+                  Invoke-RestMethod -Uri $deleteTokenUri -Method DELETE -Headers $AuthToken  -ContentType application/json
+               }
+               catch {
+                  Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to delete token `n $($_.Exception.Message)"
+               }
             }
          }
-         $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+         try {
+            $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+         }
+         catch {
+            Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retrieve monitoring tokens `n $($_.Exception.Message)"
+         }
          $body = @{'label' = $($server.guid);} | ConvertTo-Json
          if($($agent_tokens | ? {$_.label -eq $($server.guid)}).id -eq $null) { 
-            Invoke-RestMethod -Uri $tokenuri -Method POST -Headers $AuthToken -Body $body -ContentType application/json
+            try {
+               Invoke-RestMethod -Uri $tokenuri -Method POST -Headers $AuthToken -Body $body -ContentType application/json
+            }
+            catch {
+               Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to create token `n $($_.Exception.Message)"
+            }
          }
       }
    }
-   $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+   try {
+      $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
+   }
+   catch {
+      Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to retrieve monitoring tokens `n $($_.Exception.Message)"
+   }
    $environments = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "EnvironmentName") | % {($_.split("=")[1] -replace '"', "").trim()})
    foreach($environment in $environments) {
       if(Test-Path -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')) {
          if(((Get-FileHash -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')).Hash) -ne (Get-Content -Path $($d.wD, $($environment, ".hash" -join '') -join '\'))) {
             $environmentServers = $servers | ? {$_.environmentName -eq $environment}
             foreach($environmentServer in $environmentServers) {
-               Remove-Item -Path $(("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($environmentServer.guid, "*" -join '') -join '\')) -Force
-               powershell.exe $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\') -Node $($environmentServer.servername), -ObjectGuid $($environmentServer.guid), -MonitoringID $($environmentServer.guid), -MonitoringToken $($agent_tokens | ? {$_.label -eq $($environmentServer.guid)}).id
+               try {
+                  Remove-Item -Path $(("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($environmentServer.guid, "*" -join '') -join '\')) -Force
+               }
+               catch {
+                  Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to remove file `n $($_.Exception.Message)"
+               }
+               try {
+                  powershell.exe $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\') -Node $($environmentServer.servername), -ObjectGuid $($environmentServer.guid), -MonitoringID $($environmentServer.guid), -MonitoringToken $($agent_tokens | ? {$_.label -eq $($environmentServer.guid)}).id
+               }
+               catch {
+                  Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to create configuration Mof `n $($_.Exception.Message)"
+               }
             }
             Set-Content -Path $($d.wD, $($environment, ".hash" -join '') -join '\') -Value (Get-FileHash -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')).hash
          }
@@ -113,8 +179,18 @@ Function Set-TargetResource {
    if($missingConfigs) {
       foreach($missingConfig in $missingConfigs) {
          $missingEnvironment = ($servers | ? {$_.guid -eq $missingConfig}).environmentName
-         Remove-Item $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($missingConfig, "*" -join '') -join '\') -Force
-         powershell.exe $($d.wD, $d.mR, $(($servers | ? {$_.guid -eq $missingConfig}).environmentName, ".ps1" -join '') -join '\') -Node $(($servers | ? {$_.guid -eq $missingConfig}).serverName), -ObjectGuid $(($servers | ? {$_.guid -eq $missingConfig}).guid), -MonitoringID $(($servers | ? {$_.guid -eq $missingConfig}).guid), -MonitoringToken $(($agent_tokens | ? {$_.label -eq $(($servers | ? {$_.guid -eq $missingConfig}).guid)}).id)
+         try {
+            Remove-Item $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($missingConfig, "*" -join '') -join '\') -Force
+         }
+         catch {
+            Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to remove file `n $($_.Exception.Message)"
+         }
+         try {
+            powershell.exe $($d.wD, $d.mR, $(($servers | ? {$_.guid -eq $missingConfig}).environmentName, ".ps1" -join '') -join '\') -Node $(($servers | ? {$_.guid -eq $missingConfig}).serverName), -ObjectGuid $(($servers | ? {$_.guid -eq $missingConfig}).guid), -MonitoringID $(($servers | ? {$_.guid -eq $missingConfig}).guid), -MonitoringToken $(($agent_tokens | ? {$_.label -eq $(($servers | ? {$_.guid -eq $missingConfig}).guid)}).id)
+         }
+         catch {
+            Write-EventLog -LogName DevOps -Source $logSource -EntryType Error -EventId 1002 -Message "Failed to create configuration Mof `n $($_.Exception.Message)"
+         }
       }
    }
 }
