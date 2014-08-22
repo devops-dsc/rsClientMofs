8de2b39c17d391bd1973b7c480fa061872ee4ee0
LCZ2RCS1 <mark.key@rackspace.com>
2014-08-22 11:00:31 -0500

    added function to test for missing configs

--- DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
+++ DSCResources/RS_rsClientMofs/RS_rsClientMofs.psm1
@@ -14,7 +14,14 @@ Function Test-TargetResource {
       [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Ensure
    )
    . "C:\cloud-automation\secrets.ps1"
-   
+   $catalog = (Invoke-RestMethod -Uri $("https://identity.api.rackspacecloud.com/v2.0/tokens") -Method POST -Body $(@{"auth" = @{"RAX-KSKEY:apiKeyCredentials" = @{"username" = $($d.cU); "apiKey" = $($d.cAPI)}}} | convertTo-Json) -ContentType application/json)
+   $AuthToken = @{"X-Auth-Token"=($catalog.access.token.id)}
+   $environmentGuids = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "environmentGuid") | % {($_.split("=")[1] -replace '"', "").trim()})
+   $servers = @()
+   foreach($environmentGuid in $environmentGuids) {
+      $servers += (((Invoke-RestMethod -Uri $("https://prefs.api.rackspacecloud.com/v1/WinDevOps", $environmentGuid -join '/') -Method GET -Headers $AuthToken -ContentType application/json).servers).servers)
+   }
+   #((Get-Item -Path "C:\Program Files\WindowsPowerShell\DscService\Configuration\*").BaseName -notmatch "mof") -notmatch $servers.guid | % ($_) {Remove-Item -Path $(((("C:\Program Files\WindowsPowerShell\DscService\Configuration", $_) -join '\')), "*" -join '') }
    $environments = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "EnvironmentName") | % {($_.split("=")[1] -replace '"', "").trim()})
    foreach($environment in $environments) {
       if(!(Test-Path -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\'))) {
@@ -27,9 +34,17 @@ Function Test-TargetResource {
          return $false
       }
    }
-   else {
-      return $true
+   foreach($server in $servers) {
+      if($($server.serverName)) {
+         if(!(Test-Path -Path $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($server.guid, ".mof" -join '') -join '\'))) {
+            return $false
+         }
+         if(!(Test-Path -Path $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($server.guid, ".mof.checksum" -join '') -join '\'))) {
+            return $false
+         }
+      }
    }
+   return $true
 }
 Function Set-TargetResource {
    param (
@@ -64,11 +79,21 @@ Function Set-TargetResource {
       }
    }
    $agent_tokens = (Invoke-RestMethod -Uri $tokenuri -Method GET -Headers $AuthToken).values
-   #((Get-Item -Path "C:\Program Files\WindowsPowerShell\DscService\Configuration\*").BaseName -notmatch "mof") -notmatch $servers.guid | % ($_) {Remove-Item -Path $(((("C:\Program Files\WindowsPowerShell\DscService\Configuration", $_) -join '\')), "*" -join '') }
+   $missingConfig = 0
+   foreach($server in $servers) {
+      if($($server.serverName)) {
+         if(!(Test-Path -Path $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($server.guid, ".mof" -join '') -join '\'))) {
+            $missingConfig += 1
+         }
+         if(!(Test-Path -Path $("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($server.guid, ".mof.checksum" -join '') -join '\'))) {
+            $missingConfig += 1
+         }
+      }  
+   }
    $environments = (((Get-Content -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')) -match "EnvironmentName") | % {($_.split("=")[1] -replace '"', "").trim()})
    foreach($environment in $environments) {
       if(Test-Path -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')) {
-         if(((Get-FileHash -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')).Hash) -ne (Get-Content -Path $($d.wD, $($environment, ".hash" -join '') -join '\'))) {
+         if(((Get-FileHash -Path $($d.wD, $d.mR, $($environment, ".ps1" -join '') -join '\')).Hash) -ne (Get-Content -Path $($d.wD, $($environment, ".hash" -join '') -join '\')) -or $missingConfig -gt 0) {
             $environmentServers = $servers | ? {$_.environmentName -eq $environment}
             foreach($environmentServer in $environmentServers) {
                Remove-Item -Path $(("C:\Program Files\WindowsPowerShell\DscService\Configuration", $($environmentServer.guid, "*" -join '') -join '\')) -Force
